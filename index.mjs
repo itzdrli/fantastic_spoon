e7f4e0acd665d8b13cfac96f7e9ad768365833a0
itzdrli <liletian1231@outlook.com>
2024-05-09 18:59:27 +0000

    updated anonymous

--- index.mjs
+++ index.mjs
@@ -1,4 +1,4 @@
-import { Bot } from "grammy";
+import { Bot, GrammyError, HttpError } from "grammy";
 import { logger } from "./utils/logger.mjs"
 import dotenv from 'dotenv'
 
@@ -8,6 +8,20 @@ dotenv.config({ path: `./.env` });
 const token = process.env.TG_TOKEN
 
 export const bot = new Bot(token);
+
+bot.catch((err) => {
+    const ctx = err.ctx
+    logger.error(`Error while handling update ${ctx.update.update_id}:`);
+    const e = err.error;
+    if (e instanceof GrammyError) {
+        logger.error("Error in request:", e.description);
+    } else if (e instanceof HttpError) {
+        logger.error("Could not contact Telegram:", e);
+    } else {
+        logger.error("Unknown error:", e);
+    }
+})
+
 bot.command("start", (ctx) => ctx.reply("我是 Fantastic Spoon\n向我发送meme图片即可开始投稿",
     { parse_mode: "MarkdownV2" },))
 memes().then()
--- modules/memes.mjs
+++ modules/memes.mjs
@@ -16,7 +16,7 @@ export async function memes() {
         const userId = ctx.from.id.toString();
         const photoId = ctx.msg.photo.pop().file_id;
         const username = ctx.from.username
-        userImages.set(userId.toString(), { photoId, title: '', userId, username });
+        userImages.set(userId.toString(), { photoId, title: '', userId, username, anonymous: false});
 
         await ctx.reply('请输入图片的标题:');
     });
@@ -28,15 +28,27 @@ export async function memes() {
         if (userImages.has(userId)) {
             const userData = userImages.get(userId);
             userData.title = title;
-
-            await ctx.api.sendPhoto(adminUserId, userData.photoId, {
-                caption: `来自用户 ${userId} 的图片,标题: ${title}`,
-                reply_markup: new InlineKeyboard().text('批准', `approve:${userId}`).row().text('拒绝', `reject:${userId}`)
-            });
-
-            await ctx.reply('图片已发送给管理员审核。');
+            await ctx.reply(`匿名投稿?`, {
+                reply_markup: {
+                    inline_keyboard: new InlineKeyboard().text('是', 'anonymous:yes').row().text('否', 'anonymous:no')
+                    .inline_keyboard,
+                }
+            })
         }
     });
+
+    bot.callbackQuery(/anonymous:(yes|no)/, async (ctx) => {
+        const userId = ctx.from.id.toString();
+        const userData = userImages.get(userId);
+        if (ctx.match[1] === 'yes') {
+            userData.anonymous = true
+        }
+        await ctx.api.sendPhoto(adminUserId, userData.photoId, {
+            caption: `来自用户 @${userData.username}(${userId}) 的图片,\n标题: ${userData.title}\n匿名: ${userData.anonymous ? '是' : '否'}`,
+            reply_markup: new InlineKeyboard().text('批准', `approve:${userId}`).row().text('拒绝', `reject:${userId}`)
+        });
+        await ctx.answerCallbackQuery('图片已发送至管理员等待审核。');
+    })
 
     bot.callbackQuery(/^(approve|reject):(\d+)$/, async (ctx) => {
         const action = ctx.match[1];
@@ -44,20 +56,28 @@ export async function memes() {
         if (userImages.has(userId)) {
             const userData = userImages.get(userId);
 
+            let messageLink = "https://t.me/itzdtech/";
             if (action === 'approve') {
-                // Send the photo to the specified channel
-                await ctx.api.sendPhoto(channelId, userData.photoId, {
-                    caption: `# ${userData.title}\nvia @${userData.username}\n投 稿: @FantasticSpoonBot`
-                });
-                await ctx.answerCallbackQuery('图片已审核通过并发送到频道。');
-                await ctx.api.sendMessage(userId, '您的图片已审核通过并发布。');
-                await downloadAndProcessImage(userData.photoId, userData.title)
+                if (userData.anonymous) {
+                    const res = await ctx.api.sendPhoto(channelId, userData.photoId, {
+                        caption: `# ${userData.title}\n投 稿: @FantasticSpoonBot`
+                    });
+                    messageLink +=  + res.message_id;
+                } else {
+                    const res = await ctx.api.sendPhoto(channelId, userData.photoId, {
+                        caption: `# ${userData.title}\nvia @${userData.username}\n投 稿: @FantasticSpoonBot`
+                    });
+                    messageLink += res.message_id;
+                }
+                logger.info(messageLink)
+                await ctx.answerCallbackQuery('图片已审核通过并发送到频道.');
+                await ctx.api.sendMessage(userId, `您的图片已审核通过并发布.\n${messageLink}`);
+                // await downloadAndProcessImage(userData.photoId, userData.title)
             } else {
-                await ctx.answerCallbackQuery('图片已被拒绝。');
-                await ctx.api.sendMessage(userId, '您的图片未通过审核。');
+                await ctx.answerCallbackQuery('图片已被拒绝.');
+                await ctx.api.sendMessage(userId, '您的图片未通过审核.');
             }
 
-            // Clear stored photo and title
             userImages.delete(userId);
         } else {
             await ctx.answerCallbackQuery('未找到相关的图片数据。');
