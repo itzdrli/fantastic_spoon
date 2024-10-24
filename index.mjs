0c7be8958da24c289b609d4f9c870466a1fd3b7e
itzdrli <itzdrli@proton.me>
2024-10-24 22:40:13 +0200

    oi

--- index.mjs
+++ index.mjs
@@ -11,24 +11,23 @@ const token = process.env.TG_TOKEN
 
 export const bot = new Bot(token);
 
-// bot.catch((err) => {
-//     const ctx = err.ctx
-//     logger.error(`Error while handling update ${ctx.update.update_id}:`);
-//     const e = err.error;
-//     if (e instanceof GrammyError) {
-//         logger.error("Error in request:", e.description);
-//     } else if (e instanceof HttpError) {
-//         logger.error("Could not contact Telegram:", e);
-//     } else {
-//         logger.error("Unknown error:", e);
-//     }
-// })
+bot.catch((err) => {
+    const ctx = err.ctx
+    logger.error(`Error while handling update ${ctx.update.update_id}:`);
+    const e = err.error;
+    if (e instanceof GrammyError) {
+        logger.error("Error in request:", e.description);
+    } else if (e instanceof HttpError) {
+        logger.error("Could not contact Telegram:", e);
+    } else {
+        logger.error("Unknown error:", e);
+    }
+})
 
-// bot.use(generateUpdateMiddleware());
+bot.use(generateUpdateMiddleware());
 
 bot.command("help", (ctx) => ctx.reply(`
 /meme - random meme from meems.none.bot (koishi memes)
-/surl <url> - shorten a URL
 `))
 
 bot.command("start", (ctx) => ctx.reply(`
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Fantastic Spoon
+
+> A spoon developed by Itz_Dr_Li  
+> Also Koishi Unofficial Memes Bot
+
+Open Sourced on [Github](https://github.com/itzdrli/fantastic_spoon)
+
+Koishi Memes: [https://memes.none.bot](https://memes.none.bot)
+Koishi Memes Channel: [https://t.me/koimemes](https://t.me/koimemes)
