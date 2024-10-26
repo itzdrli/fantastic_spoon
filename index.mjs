c1f6e3cee4c9e9d2d0655a5034db7adcc4ff7ec3
itzdrli <itzdrli@proton.me>
2024-10-26 15:34:24 +0200

    ah

--- index.mjs
+++ index.mjs
@@ -11,21 +11,6 @@ const token = process.env.TG_TOKEN
 
 export const bot = new Bot(token);
 
-bot.catch((err) => {
-    const ctx = err.ctx
-    logger.error(`Error while handling update ${ctx.update.update_id}:`);
-    const e = err.error;
-    if (e instanceof GrammyError) {
-        logger.error("Error in request:", e.description);
-    } else if (e instanceof HttpError) {
-        logger.error("Could not contact Telegram:", e);
-    } else {
-        logger.error("Unknown error:", e);
-    }
-})
-
-bot.use(generateUpdateMiddleware());
-
 bot.command("help", (ctx) => ctx.reply(`
 /meme - random meme from meems.none.bot (koishi memes)
 `))
